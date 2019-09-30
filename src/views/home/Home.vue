6a113c2f9663af95cf93a1ba6bc7487fb4bf0929
jiguijian <jigj@landicorp.com>
2019-09-30 17:03:14 +0800

    更新一下

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,12 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 
-    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll">
+    <scroll class="content"
+            ref="scroll" 
+            :probe-type="3" 
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
       <home-swiper :banners="banners"/>
       <recommend-view :recommends="recommends"/>
       <feature-view/>
@@ -91,6 +96,9 @@
       contentScroll(position) {
         this.isShowBackTop = (-position.y) > 1000
       },
+      loadMore() {
+        this.getHomeGoods(this.currentType)
+      },
       /*
        *网络请求相关的方法
        */
@@ -104,12 +112,13 @@
       getHomeGoods(type) {
         const page = this.goods[type].page + 1
         getHomeGoods(type, page).then(res => {
-          console.log(res)
           // for(let n of res.data.list) {
           //   this.goods[type].list.push(n)
           // }
           this.goods[type].list.push(...res.data.list)
           this.goods[type].page += 1
+
+          this.$refs.scroll.finishPullUp()
         })
       },
     },
